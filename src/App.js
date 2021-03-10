ddabfc948f40d260cfee1b1a6aca2dd73e12405c
Richard Gabaree <richard.gabaree@gmail.com>
2021-03-09 21:08:41 -0600

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const getUsers = function() {
+  return axios.get("https://randomuser.me/api/?results=200&nat=us");
 }
-
-export default App;
