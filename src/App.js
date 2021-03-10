6ced336a4e837220374c5dbaaadeaae17a4647c0
Richard Gabaree <richard.gabaree@gmail.com>
2021-03-09 21:23:36 -0600

    changed App.js to API.js and added a App.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import axios from "axios";
+import React, { useState, useEffect } from "react";
+import FilterInput from "./FilterInput";
+import Table from "./Table";
+import { getUsers } from "./API";
+import "./App.css";
 
-export const getUsers = function() {
-  return axios.get("https://randomuser.me/api/?results=200&nat=us");
+function App() {
+    const [initialUser, updateAvailableUsers] = useState({});
+    const [usersToRender, updateUsersToRender] = useState({});
+
+    useEffect(() => {
+        getUsers().then(({ data: {results } }) => updateAvailableUsers(results));
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Employee Directory</h1>
+            <p> 
+                To begin your search, enter a first name inthe user input below
+            </p>
+            <FilterInput users={initialUser} updateUsers={updateUsersToRender} />
+            <Table users={usersToRender} />
+        </div>
+    );
 }
+
+export default App;
